38c3736660dc4fde2596f6ca71cd04d95ea7a39b
sarah-abraham <sarahabraham2014@gmail.com>
2024-07-17 11:39:44 +0530

    Added remove, length and clear functions to HashMap:

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ class Node{
 class LinkedList{
   constructor(){
     this.head = null;
+    this.size = 0;
   }
 
   add(key, value) {
     if (this.head === null) {
       this.head = new Node(key, value);
+      this.size++;
       return;
     }
 
@@ -30,6 +32,7 @@ class LinkedList{
     }
 
     current.next = new Node(key, value);
+    this.size++;
   }
 
   get(key) {
@@ -43,11 +46,36 @@ class LinkedList{
 
     return null; 
   }
+
+  
+  remove(key) {
+    if (this.head === null) {
+      return false;
+    }
+
+    if (this.head.key === key) {
+      this.head = this.head.next;
+      this.size--;
+      return true;
+    }
+
+    let current = this.head;
+    while (current.next !== null) {
+      if (current.next.key === key) {
+        current.next = current.next.next;
+        this.size--;
+        return true;
+      }
+      current = current.next;
+    }
+
+    return false;
+  }
 }
 class HashMap{
   constructor(bucketSize){
     this.bucketSize = bucketSize;
-    this.buckets = new Array(bucketSize).fill(null).map(() => new LinkedList());
+    this.buckets = new Array(bucketSize).map(() => new LinkedList());
   }
 
   hash(key) {
@@ -83,5 +111,23 @@ class HashMap{
     }
 
     return false;
+  }
+
+  remove(key){
+    const index = this.hash(key);
+    this.buckets[index].remove(key);
+    
+  }
+
+  length(){
+    let count = 0;
+    for(let i = 0; i<this.bucketSize; i++){
+      count += this.buckets[i].size;
+    }
+    return count;
+  }
+
+  clear(){
+    this.buckets = new Array(this.bucketSize).map(() => new LinkedList());
   }
 }
